78f7c828089a4b85c59620e65f85b70c747143ee
AkaCMD <akacmd@foxmail.com>
2025-12-23 20:34:31 +0800

    feat: add DraggableControl for draggable UI elements

---
+++ ui/DraggableControl.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+public partial class DraggableControl : Control
+{
+    private bool _isDragging = false;
+    private Vector2 _dragOffset;
+
+    public override void _GuiInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseButton)
+        {
+            if (mouseButton.ButtonIndex == MouseButton.Left)
+            {
+                if (mouseButton.Pressed)
+                {
+                    _isDragging = true;
+                    _dragOffset = GlobalPosition - GetGlobalMousePosition();
+                }
+                else
+                {
+                    _isDragging = false;
+                }
+            }
+        }
+        else if (@event is InputEventMouseMotion mouseMotion)
+        {
+            if (_isDragging)
+            {
+                GlobalPosition = GetGlobalMousePosition() + _dragOffset;
+            }
+        }
+    }
+}
