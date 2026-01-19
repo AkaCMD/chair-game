7e3409292133983ab2dc2057f9166f461d06d5b3
AkaCMD <akacmd@foxmail.com>
2026-01-19 23:14:07 +0800

    chore: add dialog about turrets

--- dialog/DialogResource.cs
+++ dialog/DialogResource.cs
@@ -6,6 +6,6 @@ public partial class DialogResource : Resource
     [Export] public string SpeakerName;
     [Export] public Texture2D SpeakerImg;
     [Export(PropertyHint.MultilineText)] public string Text;
-    [Export(PropertyHint.Range, "0.1,30.0,0.1")] public float TextSpeed;
+    [Export(PropertyHint.Range, "0.1, 0.5, 0.1")] public float TextSpeed;
     [Export(PropertyHint.Range, "-0.8, 0.8")] public float PitchOffset;
 }
